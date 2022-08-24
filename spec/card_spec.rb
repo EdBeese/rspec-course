5a9f6b5c6b6d301eca39af6e432d3866620c8f0a
Edward Beesley <edwardjbeesley@gmail.com>
2022-08-24 17:54:33 +0100

    completed card_spec - added school_spec

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,7 +1,9 @@
 class Card
-  attr_reader :type
-  def initialize(type)
-    @type = type
+  attr_reader :rank, :suit
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
   end
 end
 
