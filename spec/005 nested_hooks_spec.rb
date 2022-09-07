a5f4837bf959780122117fd73af25bbe3219998f
Edward Beesley <edwardjbeesley@gmail.com>
2022-09-07 19:08:58 +0100

    covered nested hooks

---
+++ spec/005 nested_hooks_spec.rb
@@ -0,0 +1,36 @@
+# Considering the order of execution for the following:
+RSpec.describe 'nested hooks' do
+  # This will be run once at the start of the execution
+  before(:context) do
+    puts 'Outer before context'
+  end
+
+  # This will be run once for the test in this context, and then for all of the other tests within the context block below (3 times)
+  before(:example) do
+    puts 'Outer before example'
+  end
+
+  it 'does basic maths' do
+    expect(1 + 1).to eq(2)
+  end
+
+  context 'with condition A' do
+    # This will run once before the tests in this context
+    before(:context) do
+      puts 'Inner before context'
+    end
+
+    # This will run before every test within this level
+    before(:example) do
+      puts 'Inner before example'
+    end
+
+    it 'does some more basic maths' do
+      expect(2 + 2).to eq(4)
+    end
+
+    it 'does subtraction to' do
+      expect(3 - 2).to eq(1)
+    end
+  end
+end
