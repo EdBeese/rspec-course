547e81a50346721b4a0ed6700495467134333cc0
Edward Beesley <edwardjbeesley@gmail.com>
2022-09-16 17:02:26 +0100

    covered custom testing with satisfy matcher

---
+++ spec/026_satisfy_matcher_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe 'satisfy matcher' do
+  subject { 'racecars' }
+
+  # Our custom method is checking that the word is a palindrome
+  it 'is a palindrome' do
+    expect(subject).to satisfy { |value| value == value.reverse }
+  end
+
+  # But we really want to include a custom message to explain the error
+  it 'can accept a custom error message' do
+    expect(subject).to satisfy('be a palindrome') { |value| value == value.reverse }
+  end
+end
